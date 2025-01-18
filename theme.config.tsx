b5054d6119f762e69354a44732f8191f7727e692
musti <mustafa@musti.codes>
2025-01-18 11:28:29 +0300

    update nextra config

--- theme.config.tsx
+++ theme.config.tsx
@@ -2,16 +2,16 @@ import React from 'react'
 import { DocsThemeConfig } from 'nextra-theme-docs'
 
 const config: DocsThemeConfig = {
-  logo: <span>My Project</span>,
+  logo: <span>Connes</span>,
   project: {
-    link: 'https://github.com/shuding/nextra-docs-template',
+    link: 'https://github.com/ConnesLink/documentation',
   },
   chat: {
-    link: 'https://discord.com',
+    link: null,
   },
-  docsRepositoryBase: 'https://github.com/shuding/nextra-docs-template',
+  docsRepositoryBase: 'https://github.com/ConnesLink/documentation',
   footer: {
-    text: 'Nextra Docs Template',
+    component: null,
   },
 }
 
